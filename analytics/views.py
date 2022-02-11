7d20991bef188e903a1d41ea814fd333fcfde75b
JoGorska <83362036+JoGorska@users.noreply.github.com>
2022-02-11 22:46:55 +0000

    update data object to show dynamic values

--- analytics/views.py
+++ analytics/views.py
@@ -17,21 +17,15 @@ class HelloWorld(APIView):
     """
 
     def get(self, request, format=None):
-        data = {
-            'version': 1.0,
-            'time': datetime.now(),
-            'recent_visitors': 0,
-            'all_visitors': 0,
-            'all_visits': 0,
-        }
+
         
         user_from_request = request.user
         user_id = user_from_request.id
         user_object = get_object_or_404(User, id=user_id)
-        all_visits = UserVisit.objects.all()
+        all_visits_objects = UserVisit.objects.all()
         users_already_in = []
         # checks if user already has UserVisit
-        for visit in all_visits:
+        for visit in all_visits_objects:
             users_already_in.append(visit.user)
         if user_object in users_already_in:
             # updates the UserVisit object if user already has an object
@@ -50,7 +44,18 @@ class HelloWorld(APIView):
                 user = user_object,
 
             )
-
+        # for loop to get summary of all visits from each user
+        all_visits = 0
+        for visit in all_visits_objects:
+            all_visits += visit.visits
+        
+        data = {
+            'version': 1.0,
+            'time': datetime.now(),
+            'recent_visitors': UserVisit.objects.count(),
+            'all_visitors': User.objects.count(),
+            'all_visits': all_visits,
+        }
         return Response(data)
     
             
